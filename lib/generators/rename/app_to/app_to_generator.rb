be1e0c0267a5145cddacc455368532544cf5d101
Morshed Alam <morshed201@gmail.com>
2013-06-02 15:30:54 +0600

    Add name validation and optimize code

--- lib/generators/rename/app_to/app_to_generator.rb
+++ lib/generators/rename/app_to/app_to_generator.rb
@@ -1,63 +1,75 @@
 module Rename
   module Generators
     class AppToGenerator < Rails::Generators::Base
-      argument :new_name, :type => :string, :default => "#{Rails.application.class.parent}"
+      argument :new_name, :type => :string
 
       def app_to
-        mod_name = new_name.gsub(/[^0-9A-Za-z]/, ' ').split(' ').map { |w| w.capitalize }.join('')
-
-        if mod_name.blank?
-          puts "Error:Invalid name"
-          return
-        end
-
-        new_module_name(mod_name)
-        new_directory_name(new_name)
+        return if !valid_app_name?
+        new_module_name()
+        new_directory_name()
       end
 
       private
-      def new_module_name(new_name)
+
+      def valid_app_name?
+        if new_name.to_s.strip.blank?
+          puts "Please enter new application name"
+          return false
+        elsif new_name.match(/^[A-Za-z]/).nil?
+          puts "Invalid application name"
+          return false
+        elsif new_name.size < 3
+          puts "New application name too short"
+          return false
+        elsif new_name.scan(/[0-9A-Za-z]/).size < 3
+          puts "Name should have minimum 3 alphanumeric characters"
+          return false
+        end
+
+        return true
+      end
+
+      def new_module_name()
         search_exp = /(#{Regexp.escape("#{Rails.application.class.parent}")})/m
+        module_name = new_name.gsub(/[^0-9A-Za-z]/, ' ').split(' ').map { |w| w.capitalize }.join('')
 
         in_root do
-          #Search and replace in to root
           puts "Search and Replace Module in to..."
-          Dir["*"].each do |file|
-            replace_module_in_file(file, search_exp, new_name)
-          end
 
-          #Search and replace under config
-          Dir["config/**/**/*.rb"].each do |file|
-            replace_module_in_file(file, search_exp, new_name)
+          #Search and replace module in to file
+          Dir["*", "config/**/**/*.rb"].each do |file|
+            search_and_replace_module_into_file(file, search_exp, module_name)
           end
 
           #Rename session key
           session_key_file = 'config/initializers/session_store.rb'
           search_exp = /((\'|\")_.*_session(\'|\"))/i
-          session_key = "'_#{new_name.gsub(/[^0-9A-Za-z_]/, '_').downcase}_session'"
-          replace_module_in_file(session_key_file, search_exp, session_key)
+          session_key = "'_#{module_name.gsub(/[^0-9A-Za-z_]/, '_').downcase}_session'"
+          search_and_replace_module_into_file(session_key_file, search_exp, session_key)
         end
       end
 
-      def replace_module_in_file(file, search_exp, module_name)
-        return if File.directory?(file)
-
+      def new_directory_name()
         begin
-          gsub_file file, search_exp do |m|
-            module_name
-          end
+          print "Renaming application directory..."
+
+          new_app_name = new_name.gsub(/[^0-9A-Za-z_]/, '-')
+          new_path = Rails.root.to_s.split('/')[0...-1].push(new_app_name).join('/')
+
+          File.rename(Rails.root.to_s, new_path)
+          puts "Done!"
         rescue Exception => ex
           puts "Error:#{ex.message}"
         end
       end
 
-      def new_directory_name(new_name)
+      def search_and_replace_module_into_file(file, search_exp, new_module_name)
+        return if File.directory?(file)
+
         begin
-          print "Renaming directory..."
-          new_app_name = new_name.gsub(/[^0-9A-Za-z_]/, '-')
-          new_path = Rails.root.to_s.split('/')[0...-1].push(new_app_name).join('/')
-          File.rename(Rails.root.to_s, new_path)
-          puts "Done!"
+          gsub_file file, search_exp do |m|
+            new_module_name
+          end
         rescue Exception => ex
           puts "Error:#{ex.message}"
         end
